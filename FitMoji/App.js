ac7ba5b51ed739c62c7e558a37635dcdda1dbd47
Noah Schofield <38117276+nschofield2@users.noreply.github.com>
2022-11-20 14:55:56 -0500

    Added Simple Placeholder Screen

--- FitMoji/App.js
+++ FitMoji/App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Lemon-Jelly': require('./assets/fonts/lemonjelly.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{
+            textShadowColor: '#000000',
+            textShadowRadius: '5',
+            fontFamily: 'Lemon-Jelly',
+            textAlign: 'center',
+            color: '#FFFFFF',
+            fontSize: 120,
+        }}>FitMoji</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ADD8E6',
     justifyContent: 'center',
   },
 });
