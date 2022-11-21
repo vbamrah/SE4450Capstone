607941e6bdc833e029f4e5630f6064ea3840f39a
Noah Schofield <38117276+nschofield2@users.noreply.github.com>
2022-11-21 14:19:32 -0500

    Edits to buttons

--- FitMoji/screens/EditProfile.js
+++ FitMoji/screens/EditProfile.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useEffect, useCallback } from 'react';
-import { View, Text, Button } from "react-native"
+import { useCallback } from 'react';
+import { View, Text, Pressable } from "react-native"
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
@@ -20,19 +20,35 @@ const EditProfile = ({ navigation }) => {
     }
 
     return (
-        <View style = {{flex : 1, justifyContent : 'flex-start', marginTop: 50}}>
+        <View style = {{flex : 1, justifyContent : 'flex-start'}}>
            <Text style={{
+                marginTop: 50,
                 textShadowColor: '#000000',
                 textShadowRadius: '10',
                 fontFamily: 'Lemon-Milk',
                 textAlign: 'center',
                 color: '#FFFFFF',
                 fontSize: 50,
-            }}>Edit Profile</Text>
-            <Button 
+            }}>Profile</Text>
+            <Pressable
                 onPress = {() => navigation.navigate('Home')}
-                title = 'Return'
-            />
+                style = {{
+                    marginTop: 10,
+                    backgroundColor: '#FFFFFF',
+                    width: 90,
+                    borderRadius: 10,
+                    alignSelf: 'center'
+                }}
+            >
+                <Text style = {{
+                    textShadowColor: '#000000',
+                    textShadowRadius: '2',
+                    fontFamily: 'Lemon-Milk',
+                    textAlign: 'center',
+                    color: '#000000',
+                    fontSize: 15,
+                }}>Return</Text>
+            </Pressable>
         </View>
     )
 }
--- FitMoji/screens/Home.js
+++ FitMoji/screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useEffect, useCallback } from 'react';
-import { View, Text, Button } from "react-native"
+import { useCallback } from 'react';
+import { View, Text, Pressable } from "react-native"
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
@@ -20,19 +20,35 @@ const Home = ({ navigation }) => {
     }
     
     return (
-        <View style = {{flex : 1, justifyContent : 'flex-start', marginTop: 50}}>
+        <View style = {{flex : 1, justifyContent : 'flex-start'}}>
             <Text style = {{
+                marginTop: 50,
                 textShadowColor: '#000000',
                 textShadowRadius: '10',
                 fontFamily: 'Lemon-Milk',
                 textAlign: 'center',
                 color: '#FFFFFF',
                 fontSize: 50,
-            }}>Home View</Text>
-            <Button 
+            }}>FitMoji</Text>
+            <Pressable
                 onPress = {() => navigation.navigate('Profile')}
-                title = 'Edit Profile'
-            />
+                style = {{
+                    marginTop: 10,
+                    backgroundColor: '#FFFFFF',
+                    width: 130,
+                    borderRadius: 10,
+                    alignSelf: 'center'
+                }}
+            >
+                <Text style = {{
+                    textShadowColor: '#000000',
+                    textShadowRadius: '2',
+                    fontFamily: 'Lemon-Milk',
+                    textAlign: 'center',
+                    color: '#000000',
+                    fontSize: 15,
+                }}>Edit Profile</Text>
+            </Pressable>
         </View>
     )
 }
