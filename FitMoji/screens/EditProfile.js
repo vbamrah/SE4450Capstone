f5d35b5782100a89c68979f4b63f0f0e04c3c5f3
noahschofield <71097004+noahschofield@users.noreply.github.com>
2022-11-27 18:40:31 -0500

    Added Basic Profile Saving

--- FitMoji/screens/EditProfile.js
+++ FitMoji/screens/EditProfile.js
@@ -1,11 +1,61 @@
 import React from 'react'
-import { useCallback, useState } from 'react';
-import { StyleSheet, View, Text, Pressable, ScrollView, Image, TextInput } from "react-native"
+import { useCallback, useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Pressable, ScrollView, Image, TextInput, KeyboardAvoidingView} from "react-native"
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useHeaderHeight } from '@react-navigation/elements'
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
 const EditProfile = ({ navigation }) => {
-    const [userName, setName] = useState('Username');
+    useEffect(() => {
+        getData();
+    })
+
+    const [userName, setName] = useState("Username");
+    const [email, setEmail] = useState("Email");
+    const height = useHeaderHeight()
+
+    const saveData = async() => {
+        try{
+            await AsyncStorage.setItem('username', userName);
+            await AsyncStorage.setItem('email', email);
+            alert("Profile Saved!")
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const clearUser = async() => {
+        try {
+            await AsyncStorage.removeItem('username');
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const clearEmail = async() => {
+        try {
+            await AsyncStorage.removeItem('email');
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const getData = async() => {
+        try {
+            const user = await AsyncStorage.getItem('username')
+            const email = await AsyncStorage.getItem('email')
+            if (user !== null) {
+                setName(user)
+            }
+            if (email !== null) {
+                setEmail(email)
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
     
     const [fontsLoaded] = useFonts({
         'Lemon-Milk': require('./fonts/LEMONMILK-Regular.otf'),
@@ -20,133 +70,134 @@ const EditProfile = ({ navigation }) => {
     if (!fontsLoaded) {
         return null;
     }
-
-    return (
-        <View style = {{flex: 1}}>
-            <ScrollView>
-                <View style = {{
-                    borderRadius: 10,
-                    padding: 10,
-                    width: '100%',
-                    backgroundColor: '#b5e8ff',
-                    height: 225,
-                    shadowColor: '#8E8E8E',
-                    shadowOpacity: '100%',
-                    shadowOffset: {width: 0, height: 1},
-                    borderRadius: 30}}>
-                    <Text style={{
-                    marginTop: 50,
-                    textShadowColor: '#000000',
-                    textShadowRadius: '8',
-                    fontFamily: 'Lemon-Milk',
-                    textAlign: 'center',
-                    color: '#FFFFFF',
-                    fontSize: 40,
-                    }}>Profile</Text>
-                    <Pressable
-                        onPress = {() => navigation.navigate('Home')}
-                        style = {{
-                            marginTop: -45,
-                            marginRight: 300,
-                            width: 90,
-                            alignSelf: 'center'
-                        }}
-                    >
-                        <Text style = {{
-                            textShadowColor: '#Bfbfbf',
-                            textShadowRadius: '2',
+            return (
+                <KeyboardAvoidingView 
+                keyboardVerticalOffset = {height - 120}
+                behavior = "position"
+                style = {{
+                    flex: 1,
+                    paddingBottom: 20
+                }}>
+                    <ScrollView>
+                        <View style = {{
+                            borderRadius: 10,
+                            padding: 10,
+                            width: '100%',
+                            backgroundColor: '#b5e8ff',
+                            height: 225,
+                            shadowColor: '#8E8E8E',
+                            shadowOpacity: '100%',
+                            shadowOffset: {width: 0, height: 1},
+                            borderRadius: 30}}>
+                            <Text style={{
+                            marginTop: 50,
+                            textShadowColor: '#000000',
+                            textShadowRadius: '8',
                             fontFamily: 'Lemon-Milk',
                             textAlign: 'center',
-                            color: '#000000',
-                            fontSize: 15,
-                        }}>Home</Text>
-                    </Pressable>
-                    <Pressable
-                        //onPress = {() => Save}
-                        style = {{
-                            marginTop: -20,
-                            marginRight: -300,
-                            width: 90,
-                            alignSelf: 'center'
-                        }}
-                    >
+                            color: '#FFFFFF',
+                            fontSize: 40,
+                            }}>Profile</Text>
+                            <Pressable
+                                onPress = {() => navigation.navigate('Home')}
+                                style = {{
+                                    marginTop: -45,
+                                    marginRight: 300,
+                                    width: 90,
+                                    alignSelf: 'center'
+                                }}
+                            >
+                                <Text style = {{
+                                    textShadowColor: '#Bfbfbf',
+                                    textShadowRadius: '2',
+                                    fontFamily: 'Lemon-Milk',
+                                    textAlign: 'center',
+                                    color: '#000000',
+                                    fontSize: 15,
+                                }}>Home</Text>
+                            </Pressable>
+                            <Pressable
+                                onPress = {() => saveData()}
+                                style = {{
+                                    marginTop: -20,
+                                    marginRight: -300,
+                                    width: 90,
+                                    alignSelf: 'center'
+                                }}
+                            >
+                                <Text style = {{
+                                    textShadowColor: '#Bfbfbf',
+                                    textShadowRadius: '2',
+                                    fontFamily: 'Lemon-Milk',
+                                    textAlign: 'center',
+                                    color: '#000000',
+                                    fontSize: 15,
+                                }}>Save</Text>
+                            </Pressable>
+                        </View>
+                        <View style = {{
+                            alignItems: 'center',
+                            shadowColor: '#8E8E8E',
+                            shadowOpacity: '100%',
+                            paddingBottom: 10,
+                            shadowOffset: {width: 0, height: 1}
+                            }}>
+                            <Image source = {require('../assets/images/userprofile.png')}
+                                style = {{
+                                    width: 140,
+                                    height: 140,
+                                    borderRadius: 100,
+                                    marginTop: -75,
+                                }}>
+                            </Image>
+                        </View>
                         <Text style = {{
-                            textShadowColor: '#Bfbfbf',
-                            textShadowRadius: '2',
+                            alignSelf: 'center',
+                            fontSize: 20,
+                            paddingTop: 20,
                             fontFamily: 'Lemon-Milk',
-                            textAlign: 'center',
-                            color: '#000000',
-                            fontSize: 15,
-                        }}>Save</Text>
-                    </Pressable>
-                </View>
-                <View style = {{
-                    alignItems: 'center',
-                    shadowColor: '#8E8E8E',
-                    shadowOpacity: '100%',
-                    paddingBottom: 10,
-                    shadowOffset: {width: 0, height: 1}
-                    }}>
-                    <Image source = {require('../assets/images/userprofile.png')}
-                        style = {{
-                            width: 140,
-                            height: 140,
-                            borderRadius: 100,
-                            marginTop: -75,
                         }}>
-                    </Image>
-                </View>
-                <Text style = {{
-                    alignSelf: 'center',
-                    fontSize: 20,
-                    fontFamily: 'Lemon-Milk',
-                }}>
-                    {userName}
-                </Text>
-                <Text style = {{
-                    marginTop: 40,
-                    padding: 20,
-                    paddingLeft: 40,
-                    fontSize: 10,
-                    fontFamily: 'Lemon-Milk',
-                }}>
-                    Username
-                </Text>
-                <View>
-                    <TextInput 
-                        style = {styles.input}
-                        placeholder = 'Enter Username'
-                        onChangeText = {(val) => setName(val)}/>
-                </View>
-                <Text style = {{
-                    padding: 20,
-                    paddingLeft: 40,
-                    fontSize: 10,
-                    fontFamily: 'Lemon-Milk',
-                }}>
-                    Email
-                </Text>
-                <View>
-                    <TextInput 
-                        style = {styles.input}
-                        placeholder = 'Enter Email'/>
-                </View>
-                <Text style = {{
-                    padding: 20,
-                    paddingLeft: 40,
-                    fontSize: 10,
-                    fontFamily: 'Lemon-Milk',
-                }}>
-                    Password
-                </Text>
-                <View>
-                    <TextInput 
-                        style = {styles.input}
-                        placeholder = 'Enter Password'/>
-                </View>
-            </ScrollView>
-        </View>
-    )
+                            {userName}
+                        </Text>
+                        <Text style = {{
+                            alignSelf: 'center',
+                            fontSize: 10,
+                            paddingBottom: 10,
+                            fontFamily: 'Lemon-Milk',
+                        }}>
+                            {email}
+                        </Text>
+                        <Text style = {styles.inputLabel}>
+                            Username
+                        </Text>
+                        <View>
+                            <TextInput 
+                                style = {styles.input}
+                                placeholder = {userName}
+                                onFocus = {() => clearUser()}
+                                onChangeText = {(val) => setName(val)}/>
+                        </View>
+                        <Text style = {styles.inputLabel}>
+                            Email
+                        </Text>
+                        <View>
+                            <TextInput 
+                                style = {styles.input}
+                                placeholder = {email}
+                                onFocus = {() => clearEmail()}
+                                onChangeText = {(val) => setEmail(val)}/>
+                        </View>
+                        <Text style = {styles.inputLabel}>
+                            Password
+                        </Text>
+                        <View>
+                            <TextInput 
+                                style = {styles.input}
+                                placeholder = 'Enter Password'/>
+                        </View>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            );
 }
 
 const styles = StyleSheet.create({
@@ -163,6 +214,12 @@ const styles = StyleSheet.create({
         paddingBottom: 20,
         borderRadius: 20,
         marginBottom: 20,
+    },
+    inputLabel: {
+        padding: 20,
+        paddingLeft: 40,
+        fontSize: 10,
+        fontFamily: 'Lemon-Milk',
     }
 })
 
