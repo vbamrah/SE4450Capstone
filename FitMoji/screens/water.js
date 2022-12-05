fb6672bc24e16b163b61bf88f4480f1d77b3c0cc
Izzy Alfred <71096849+ialfred2@users.noreply.github.com>
2022-12-05 12:20:52 -0500

    Update water.js

--- FitMoji/screens/water.js
+++ FitMoji/screens/water.js
@@ -1,1 +1,129 @@
+import { useNavigation } from '@react-navigation/native';
+import React, {useEffect, useState, useCallback} from 'react'
+import { getDatabase, ref, set } from "firebase/database";
+import { DatePickerInput } from 'react-native-paper-dates';
+import {View,
+TextInput,
+  Text,
+  StyleSheet,
+  Pressable,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native'
+import { auth } from '../firebase';
+import { database } from '../firebase';
 
+import {
+    enGB,
+    registerTranslation,
+  } from 'react-native-paper-dates' 
+registerTranslation('en-GB', enGB)
+
+const water = ({navigation}) => {
+    const [waterAmount, setWater] = useState('');
+    
+
+    const validateInputs = () => {
+        if (waterAmount == '') {
+            alert('Invalid input');
+        }
+    
+    }
+
+    
+   
+    
+      
+    const locale = 'en-GB'
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        
+        <View style={styles.inputContainer}>
+            <Text style = {{
+                    textShadowColor: '#000000',
+                    textShadowRadius: '10',
+                    fontFamily: 'Lemon-Milk',
+                    textAlign: 'center',
+                    color: '#FFFFFF',
+                    fontSize: 50,
+                    marginBottom: 30
+                }}>Water Tracker</Text>
+              <Text style={styles.goalText}>Goal: 2 Litres</Text>
+              <View>
+                <TextInput placeholder='Enter amount of water'
+                    style={styles.waterInput}
+                    value= {waterAmount}
+                    keyboardType="numeric"
+                    maxLength={3}
+                    />
+              </View>
+        </View>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity 
+            style={styles.button}
+            onPress={validateInputs}
+            >
+                <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    )
+}
+
+const styles = StyleSheet.create({
+     input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputContainer: {
+        width: '80%',
+
+    },
+    waterInput: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+        width: "100%"
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40
+    },
+    button: {
+        backgroundColor: "#42A5F5",
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16
+    },
+    header: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+        marginTop: 10
+    }
+});
+
+export default water;
