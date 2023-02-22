a0bbe1677b56053c6bed354eed1dcf94a3e7ed4b
Izzy Alfred <71096849+ialfred2@users.noreply.github.com>
2023-02-22 17:57:43 -0500

    Added to calendar.js

--- FitMoji/screens/Calendar.js
+++ FitMoji/screens/Calendar.js
@@ -1,1 +1,130 @@
 
+import { useNavigation } from '@react-navigation/native';
+import React, {useEffect, useState, useCallback} from 'react'
+import { getDatabase, ref, set } from "firebase/database";
+import { Header } from "react-native/Libraries/NewAppScreen"
+import {View,
+TextInput,
+  Text,
+  StyleSheet,
+  Pressable,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native'
+import { auth } from '../firebase';
+import { database } from '../firebase';
+
+import {
+    enGB,
+    registerTranslation,
+  } from 'react-native-paper-dates' 
+registerTranslation('en-GB', enGB)
+
+const date = ({navigation}) => {
+    const [date, setDate] = useState('');
+    
+
+    const validateInputs = () => {
+        if (selectedDate == '') {
+            alert('Invalid input');
+        }
+    
+    }
+
+    
+   
+    
+      
+    const locale = 'en-GB'
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        
+        <View style={styles.inputContainer}>
+            <Text style = {{
+                    textShadowColor: '#000000',
+                    textShadowRadius: '10',
+                    fontFamily: 'Lemon-Milk',
+                    textAlign: 'center',
+                    color: '#FFFFFF',
+                    fontSize: 50,
+                    marginBottom: 30
+                }}>Calendar</Text>
+              <Text style={styles.goalText}></Text>
+              <View>
+                <TextInput placeholder='Chose the date '
+                    style={styles.waterInput}
+                    value= {selectedDate}
+                    keyboardType="numeric"
+                    maxLength={10}
+                    />
+              </View>
+        </View>
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity 
+            style={styles.button}
+            onPress={validateInputs}
+            >
+                <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    )
+}
+
+const styles = StyleSheet.create({
+     input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputContainer: {
+        width: '80%',
+
+    },
+    waterInput: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+        width: "100%"
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40
+    },
+    button: {
+        backgroundColor: "#42A5F5",
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16
+    },
+    header: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+        marginTop: 10
+    }
+});
+
+export default calendar;
