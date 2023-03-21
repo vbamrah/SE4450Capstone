ffb16b4f80ece85757241264230a52c7b7e48b83
Noah Schofield <71097004+noahschofield@users.noreply.github.com>
2023-03-21 11:31:14 -0400

    fixed user manual linear gradient

--- FitMoji/screens/UserManual.js
+++ FitMoji/screens/UserManual.js
@@ -19,62 +19,71 @@ import {
 import { auth } from '../firebase';
 import { database } from '../firebase';
 import { LinearGradient } from 'expo-linear-gradient';
-
-
+import { useHeaderHeight } from '@react-navigation/elements'
 
 function UserManual() {
+
+    const height = useHeaderHeight()
+
     return (
-        <ScrollView style={{ flex: 1 }}>
+        <KeyboardAvoidingView
+            keyboardVerticalOffset={height - 120}
+            behavior="position"
+            style={{
+                flex: 1,
+                paddingBottom: 20
+            }}>
             <LinearGradient colors={['#b5e8ff', '#ffffff']} style={{
                 position: 'absolute',
                 left: 0,
                 right: 0,
                 bottom: 0,
                 top: 0,
-                flex: 1
             }}></LinearGradient>
-            <View style={{
-                top: '5%'
-            }}>
-                <Text style={styles.heading}>User Manual</Text>
-                <Text style={styles.subheading}>Introduction</Text>
+            <ScrollView>
+                <View style={{
+                    top: '5%'
+                }}>
+                    <Text style={styles.heading}>User Manual</Text>
+                    <Text style={styles.subheading}>Introduction</Text>
 
-                <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
-                    FitMoji is designed to help you lead a healthy and balanced lifestyle by providing engaging tools
-                    to track wellness, fitness, and nutrition. This user manual will guide you through the app's features
-                    to help you get the most out of your FitMoji experience.
-                </Text>
-                <Text style={styles.subheading}>Getting Started</Text>
-                <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
-                    To use FitMoji, you will need to download it from the App Store or Google Play Store and create
-                    an account. Once you have installed the app, follow these steps to get started: {'\n'}
-                    {'\t'} 1. Launch the app on your mobile device. {'\n'}
-                    {'\t'} 2. Sign up for an account using your email. {'\n'}
-                    {'\t'} 3. Set up your avatar by taking a photo or {'\n'}{'\t'}selecting your desired customizations. {'\n'}
-                    {'\t'} 4. Complete your profile by providing basic {'\n'}{'\t'}information about yourself, such as your age, {'\n'}{'\t'}gender, height, and weight.
-                </Text>
-                <Text style={styles.subheading}>Features</Text>
-                <Text style={styles.subheading2}>Avatar</Text>
-                <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
-                    Your personalized avatar can be used to track your progress and customize your FitMoji appearance. You can edit
-                    your avatar through the Edit Profile button located at the top of the Home Page. Any changes or edits made to
-                    your avatar will be reflected on the Home Page screen.
-                </Text>
-                <Text style={styles.subheading2}>Tracking</Text>
-                <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
-                    FitMoji allows you to track your wellness goals by logging your workouts, meals,
-                    sleep schedule, and other activities. Tracking for each category is entered on that category page, which
-                    can be accessed using the buttons located at the bottom of the home page.
-                </Text>
-                <Text style={styles.subheading2}>Goal Setting</Text>
-                <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
-                    FitMoji allows you to set managable wellness goals for fitness, nutrition, water, and sleep based on your biometric
-                    information. Goals for each category can be set on the category's page, which can be accessed using the
-                    buttons located at the bottom of the home page.
-                    {'\n'}{'\n'}
-                </Text>
-            </View>
-        </ScrollView>
+                    <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
+                        FitMoji is designed to help you lead a healthy and balanced lifestyle by providing engaging tools
+                        to track wellness, fitness, and nutrition. This user manual will guide you through the app's features
+                        to help you get the most out of your FitMoji experience.
+                    </Text>
+                    <Text style={styles.subheading}>Getting Started</Text>
+                    <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
+                        To use FitMoji, you will need to download it from the App Store or Google Play Store and create
+                        an account. Once you have installed the app, follow these steps to get started: {'\n'}
+                        {'\t'} 1. Launch the app on your mobile device. {'\n'}
+                        {'\t'} 2. Sign up for an account using your email. {'\n'}
+                        {'\t'} 3. Set up your avatar by taking a photo or {'\n'}{'\t'}selecting your desired customizations. {'\n'}
+                        {'\t'} 4. Complete your profile by providing basic {'\n'}{'\t'}information about yourself, such as your age, {'\n'}{'\t'}gender, height, and weight.
+                    </Text>
+                    <Text style={styles.subheading}>Features</Text>
+                    <Text style={styles.subheading2}>Avatar</Text>
+                    <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
+                        Your personalized avatar can be used to track your progress and customize your FitMoji appearance. You can edit
+                        your avatar through the Edit Profile button located at the top of the Home Page. Any changes or edits made to
+                        your avatar will be reflected on the Home Page screen.
+                    </Text>
+                    <Text style={styles.subheading2}>Tracking</Text>
+                    <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
+                        FitMoji allows you to track your wellness goals by logging your workouts, meals,
+                        sleep schedule, and other activities. Tracking for each category is entered on that category page, which
+                        can be accessed using the buttons located at the bottom of the home page.
+                    </Text>
+                    <Text style={styles.subheading2}>Goal Setting</Text>
+                    <Text style={{ marginHorizontal: 20, marginTop: 10 }}>
+                        FitMoji allows you to set managable wellness goals for fitness, nutrition, water, and sleep based on your biometric
+                        information. Goals for each category can be set on the category's page, which can be accessed using the
+                        buttons located at the bottom of the home page.
+                        {'\n'}{'\n'}
+                    </Text>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
 
